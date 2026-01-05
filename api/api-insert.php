231ff3fe7e14d951ccdb66b1d05700e55431b119
Mohd Intikhab <intikhab@samarth.ac.in>
2026-01-05 15:56:02 +0530

    fixes

--- api/api-insert.php
+++ api/api-insert.php
@@ -8,12 +8,13 @@
     $studdent_age = $data['age'];
     $student_email = $data['email'];
     $student_mobile = $data['mobile'];
-    $student_cource = $data['course'];
+    $student_cource = strtoupper($data['course']);
     $student_status = $data['status'];
 
 
     $sql = "INSERT INTO `students`( `name`, `age`, `email`, `mobile`, `course`, `status`) 
             VALUES ('{$student_name}', '{$studdent_age}', '{$student_email}', '{$student_mobile}', '{$student_cource}', '{$student_status}');";
+            
     if(mysqli_query($conn, $sql)){
         echo json_encode(array('message' => 'Student Added Successfully.', 'status' => true));
     } else {
